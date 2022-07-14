93a54bbaeeb1eba575f3a1133b05fc7f1ceac47e
prajaktahadole <prajaktahadole@gmail.com>
2022-07-14 11:36:41 +0530

    app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
 import './App.css'
-import AllRoutes from './Components/Routes/AllRoutes'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-        <AllRoutes></AllRoutes>
+    
     </div>
   )
 }
